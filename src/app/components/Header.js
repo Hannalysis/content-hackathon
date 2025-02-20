505b3cb5e04fa7c3487fd7ff3b37e4abdd3a2594
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-02-20 23:25:29 +0000

    Added styling to Decorator part of Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,7 +3,7 @@ import styles from "../styles/Header.module.css"
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>TypeScript Decorators</h1>
+      <h1>TypeScript <span className = {styles.decorate}>Decorators</span></h1>
     </header>
   )
 }
